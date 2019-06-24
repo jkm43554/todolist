e0282838aa4322764912739682a5dfd49daa0630
Jacob Martin <you@example.com>
2019-06-24 18:03:25 +0000

    update ux

--- public/scripts/tdList.js
+++ public/scripts/tdList.js
@@ -1,23 +1,23 @@
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
-})
+});
 
 
-$("ul").on("click", "span", function(event){
+$("ul").on("click", ".listspan", function(event){
 	$(this).parent().fadeOut(200, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
-})
+});
 
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
-		var todoText = $(this).val()
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + $(this).val() + "</li>")		
+		var todoText = $(this).val();
+		$("ul").append("<li><span class='listspan'><span class='trash'>delete</span></span> " + $(this).val() + "</li>");		
 		$(this).val("");
 	}
-})
+});
 
-$(".fa-plus").click(function(){
+$(".plus").click(function(){
 	$("input[type='text']").fadeToggle();
-})
+});
